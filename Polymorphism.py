9de02b7968c5acd2f3c2f2db0caef0cb5b62da4e
Error-404 <notabuzar@gmail.com>
2024-06-30 16:17:40 +0500

    Create a file on the topic of polymorphism

---
+++ Polymorphism.py
@@ -0,0 +1,6 @@
+# Polymorphism : Same Object have different behaviour
+# Two methods of polymorphism: 1- Overloading. 2- Overriding 
+
+# same function different behaviour 
+print(len("Abuzar")) # 6
+print(len(["Abuzar","Ahmad"])) # 2 
