b2d4437567336004035a768a1bb8210a7dffbd75
Error-404 <notabuzar@gmail.com>
2024-06-30 12:01:37 +0500

    Updating the Abstraction and Encapsulation File

--- Abstraction&Encapsulation.py
+++ Abstraction&Encapsulation.py
@@ -1,2 +1,11 @@
 # ================= Encapsulation ====================
+# DEFINATION : 
+# Encapsulation is one of the fundamental concepts in object-oriented programming (OOP).
+# It describes the idea of wrapping data and the methods that work on data within one unit. 
+# This puts restrictions on accessing variables and methods directly and can prevent the accidental modification of data
+
+
 # =================== Abstraction ====================
+# Abstraction is used to hide the internal functionality of the function from the users. 
+# The users only interact with the basic implementation of the function, but inner working is hidden. 
+# User is familiar with that "what function does" but they don't know "how it does."
