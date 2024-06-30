57c5f1f0f0797db052845f7784215217bc58b1cf
Error-404 <notabuzar@gmail.com>
2024-06-30 14:36:04 +0500

    Encapsulation Understanding Using python CODE

--- Abstraction/main.py
+++ Abstraction/main.py
@@ -1,6 +1,7 @@
 # Abstraction is used to hide the internal functionality of the function from the users. 
 # The users only interact with the basic implementation of the function, but inner working is hidden. 
 # User is familiar with that "what function does" but they don't know "how it does."
+# For better understanding you can also watch a youtube vide : https://www.youtube.com/watch?v=e8xctqrzSSA&t=419s
 import ADD , SUB # Import the classes 
 ADD.add() # Hide the functionailty this is called Abstraction 
 SUB.sub() # Hide the functionailty this is called Abstraction 
