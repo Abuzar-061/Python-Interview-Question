dc3be5bc7e10e14f679beba9b70319f60e0abc36
Error-404 <notabuzar@gmail.com>
2024-07-07 18:34:03 +0500

    Write comments on the Find_MissingNumber.py File for better understanding

--- Finding_missingNumber.py
+++ Finding_missingNumber.py
@@ -1,3 +1,7 @@
+# Find the missing number in the list of natural number
+# Consider we have 8 natural numbers 1 2 3 4 6 7 8  
+# In this we have to found which natural number is missing 
+
 def find_missing_number(numbers):
     n = 8  # Since we know the numbers are from 1 to 8
     total_sum = n * (n + 1) // 2  # Sum of first n natural numbers
