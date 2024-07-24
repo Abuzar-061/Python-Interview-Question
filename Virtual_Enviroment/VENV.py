d966160e4e7419b1805c3cf5d1d20f243c4467c7
Error-404 <notabuzar@gmail.com>
2024-07-24 11:36:04 +0500

    Add a virtual Environment for understanding

---
+++ Virtual_Enviroment/VENV.py
@@ -0,0 +1,9 @@
+# pip install virtualenv
+# virtualenv myenv
+# myenv\Scripts\activate
+# Create a file name requirements.txt
+# pip install -r requirements.txt
+# Now when you install the new modue it'll install within the virtual environment
+# pip install <module_name>
+# pip freeze > requirements.txt # It'll save the module in the requirements.txt file  
+# deactivate # It'll deactivate the virtual environment
