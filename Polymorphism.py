d1a4c0859fa0390ada481ec463442b46c109fd13
Error-404 <notabuzar@gmail.com>
2024-08-05 10:56:37 +0500

    Change the placement of overloading and overriding

--- Polymorphism.py
+++ Polymorphism.py
@@ -2,6 +2,19 @@
 # Two methods of polymorphism: 1- Overloading. 2- Overriding 
 # Overloading allows multiple methods in the same class to have the same name but different parameters (different type or number of parameters).
 # Overriding occurs when a subclass provides a specific implementation for a method that is already defined in its superclass.
+
+# Polymorphism with Method Overloading 
+class MathOperations:
+    def add(self, a, b=0, c=0):
+        return a + b + c
+
+# Instantiate object
+math_ops = MathOperations()
+# Demonstrate polymorphism-like behavior using default arguments
+print(math_ops.add(1))       # Output: 1
+print(math_ops.add(1, 2))    # Output: 3
+print(math_ops.add(1, 2, 3)) # Output: 6
+
 # Polymorphism with Method Overriding 
 class Parent:
     def greet(self): 
@@ -15,16 +28,3 @@ class Child(Parent):
 # Usage
 c = Child()
 c.greet()  # Output: Hello from Child
-
-# Polymorphism with Method Overloading 
-class MathOperations:
-    def add(self, a, b=0, c=0):
-        return a + b + c
-
-# Instantiate object
-math_ops = MathOperations()
-
-# Demonstrate polymorphism-like behavior using default arguments
-print(math_ops.add(1))       # Output: 1
-print(math_ops.add(1, 2))    # Output: 3
-print(math_ops.add(1, 2, 3)) # Output: 6
--- Practice.py
+++ Practice.py
@@ -1,8 +1,12 @@
-value = input("Enter the String Value: ")
-output = []
-for i in value:
 
-    if i not in output:
-        output.append(i)
-outputs = ' '.join(output)
-print(outputs) 
+def flatten(lst):
+    flat_list = []
+    for item in lst:
+        if isinstance(item, int):
+            flat_list.append(item)
+        else:
+            flat_list.extend(flatten(item))
+    return flat_list
+
+nestedlist = [1, 2, 3, [4, 5], [[6, 7]]]
+print(flatten(nestedlist))
