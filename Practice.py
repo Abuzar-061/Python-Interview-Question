e63d1b3c3c7375d1427705f315c70a6f640abdec
Error-404 <notabuzar@gmail.com>
2024-10-10 17:49:34 +0500

    Initial Commit

--- Practice.py
+++ Practice.py
@@ -1,13 +1,29 @@
-import winsound
-import pyttsx3
-import keyboard
+# Insert a value (No Duplicate)
+# Removing a value 
+# GetRandom a value that is already inserted in the list 
 
-speak = pyttsx3.init()
+import random
 
-speak.say("Bomb Has been Planted Abuzar Run !")
-speak.runAndWait()
-print("Press ESC to exit")
-while True:
-    winsound.Beep(1000,500)
-    if keyboard.is_pressed('esc'):
-        break
+class Start:
+    def __init__(self):
+        self.name = input("Enter your name : ")
+        self.list = []
+        if self.name not in self.list:
+            self.list.append(self.name)
+            print("Value Inserted")
+        else:
+            print("Value Already Exits")
+
+    def removed(self):
+        value = input("Enter the name to remove it from the list : ")
+        if value in self.list:
+            self.list.remove(value)
+            print("Value removed")
+        else:
+            print("Value not exists in the list")
+    def getrandom(self):
+        return random.choice([self.list])
+
+obj = Start()
+obj.removed()
+print(obj.getrandom())
